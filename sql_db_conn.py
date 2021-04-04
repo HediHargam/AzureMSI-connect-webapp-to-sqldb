3684e238bf9ee9109d3229c82733c2de3b5c70df
HediHargam <60517481+HediHargam@users.noreply.github.com>
2021-04-04 14:36:22 +0200

    add authentication string

--- sql_db_conn.py
+++ sql_db_conn.py
@@ -6,26 +6,26 @@ import struct
 from logzero import logger
 
 
-def get_token_with_msi(resource_uri: str):
-    "Retrieve token in the right format (binary structure) for SQL connection with SQL_COPT_SS_ACCES_TOKEN attribute"
-
-    identity_endpoint = os.environ["IDENTITY_ENDPOINT"]
-    identity_header = os.environ["IDENTITY_HEADER"]
-
-    token_auth_uri = (
-        f"{identity_endpoint}?resource={resource_uri}&api-version=2019-08-01"
-    )
-    head_msi = {"X-IDENTITY-HEADER": identity_header}
-
-    resp = requests.get(token_auth_uri, headers=head_msi)
-    access_token = bytes(resp.json()["access_token"], "utf-8")
-    exptoken = b""
-    for i in access_token:
-        exptoken += bytes({i})
-        exptoken += bytes(1)
-    struct_token = struct.pack("=i", len(exptoken)) + exptoken
-
-    return struct_token
+# def get_token_with_msi(resource_uri: str):
+#     "Retrieve token in the right format (binary structure) for SQL connection with SQL_COPT_SS_ACCES_TOKEN attribute"
+#
+#     identity_endpoint = os.environ["IDENTITY_ENDPOINT"]
+#     identity_header = os.environ["IDENTITY_HEADER"]
+#
+#     token_auth_uri = (
+#         f"{identity_endpoint}?resource={resource_uri}&api-version=2019-08-01"
+#     )
+#     head_msi = {"X-IDENTITY-HEADER": identity_header}
+#
+#     resp = requests.get(token_auth_uri, headers=head_msi)
+#     access_token = bytes(resp.json()["access_token"], "utf-8")
+#     exptoken = b""
+#     for i in access_token:
+#         exptoken += bytes({i})
+#         exptoken += bytes(1)
+#     struct_token = struct.pack("=i", len(exptoken)) + exptoken
+#
+#     return struct_token
 
 
 def get_data_from_sql_db(
@@ -33,11 +33,16 @@ def get_data_from_sql_db(
 ) -> pd.DataFrame:
     """"Get data from SQL database with MSI Authentication and return df from executed query"""
 
-    token = get_token_with_msi(resource_uri="https://database.windows.net")
+    # token = get_token_with_msi(resource_uri="https://database.windows.net")  # May be useful for PostrgresSQL
 
     with pyodbc.connect(
-        "Driver=" + driver + ";Server=" + server + ";PORT=1433;Database=" + database,
-        attrs_before={1256: bytearray(token)},
+        "Driver="
+        + driver
+        + ";Server="
+        + server
+        + ";PORT=1433;Database="
+        + database
+        + ";Authentication=ActiveDirectoryMsi",
     ) as conn:
 
         logger.info("Successful connection to database")
